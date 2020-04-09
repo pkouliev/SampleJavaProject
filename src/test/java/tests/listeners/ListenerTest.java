d90d2b932e0ada4704d9614d1e88dfad63957fd2
pkouliev <pkouliev@gmail.com>
2020-04-08 21:23:21 -0400

    Listeners

---
+++ src/test/java/tests/listeners/ListenerTest.java
@@ -0,0 +1,30 @@
+package tests.listeners;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+//@Listeners(CustomListeners.class)
+public class ListenerTest {
+
+    @Test
+    void test1() {
+        System.out.println("This is Test1");
+        Assert.assertEquals("A","A");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("This is Test2");
+        Assert.assertEquals("A","B");
+    }
+
+    @Test
+    void test3() {
+
+        System.out.println("This is Test3");
+        throw new SkipException("This is skip exception...");
+
+    }
+}
